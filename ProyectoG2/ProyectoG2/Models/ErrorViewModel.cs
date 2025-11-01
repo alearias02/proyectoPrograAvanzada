7f8ae5f43b9d9a9427746762a12b1fdaff744b0d
Caleb210 <bismer28@gmail.com>
2025-10-31 18:22:31 -0600

    Revert "Actividad  PROGRATHON"
    This reverts commit 78a5f9bb7ecbab7fcf99effd3a89e1e08db6e109.

--- ProyectoG2/ProyectoG2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProyectoG2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
